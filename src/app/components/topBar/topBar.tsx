fc87ab6d6550e40a8755980c64cd39bf00a6b318
Edgar Aldana <eddaldana95@gmail.com>
2025-04-20 18:36:17 -0600

    add: button "Más detalles" 💄

--- src/app/components/topBar/topBar.tsx
+++ src/app/components/topBar/topBar.tsx
@@ -7,20 +7,20 @@ export const TopBar = () => {
 
         <header className="flex h-full flex-col justify-center items-center text-center text-white">
 
-            <div className="w-full flex flex-col justify-center items-center p-1">
+            <div className="w-full flex flex-col justify-center items-center">
 
                 <div className="font-['weddingStarlightFont'] text-[75px] sm:text-[120px]">Angélica y Edgar</div>
-                <div className="font-['weddingScriptFont'] text-[60px] sm:text-[90px] mb-4">Nuestra boda</div>
-                <img className="w-[120px] sm:w-[170px]" src="/media/images/rings.png" alt="" />
+                <div className="font-['weddingScriptFont'] text-[60px] sm:text-[90px]">Nuestra boda</div>
+                <img className="w-[160px] sm:w-[180px]" src="/media/images/rings-sunflower.png" alt="" />
 
-                <div className="w-[90%] h-[80px] sm:w-[35%] flex justify-center items-center bg-[#ffffff10] backdrop-blur-[10px] rounded-lg mt-2 mb-9">
-                    <p className="text-[14px] sm:text-[18px] text-white drop-shadow-lg bg-black/20 px-3 py-1 rounded font-semibold">El día más especial de nuestra vida llegará pronto y esperamos que nos puedas acompañar.</p>
+                <div className="w-[90%] h-[80px] sm:w-[35%] flex justify-center items-center bg-[#ffffff10] backdrop-blur-[10px] rounded-lg -mt-4 mb-6">
+                    <p className="text-[13px] sm:text-[18px] text-white drop-shadow-lg bg-black/20 px-3 py-1 rounded font-semibold">El día más especial de nuestra vida llegará pronto y esperamos que nos puedas acompañar.</p>
                 </div>
 
-                <div className="w-[90%] h-[80px] sm:w-[35%] sm:h-[100px] flex justify-center items-center border-2 border-white border-dashed rounded-full p-2">
+                <div className="w-[90%] h-[60px] sm:w-[40%] sm:h-[100px] flex justify-center items-center border-2 border-white border-dashed rounded-full p-2">
 
                     <h3 className="text-[13px] sm:text-[22px]">El evento se llevará a cabo en el estado de Morelos</h3>
-                    <img src="/media/images/morelos.svg" alt="" className="w-[75px] sm:w-[120px]"/>
+                    <img src="/media/images/morelos.svg" alt="" className="w-[55px] sm:w-[120px]" />
 
                 </div>
 
@@ -34,12 +34,28 @@ export const TopBar = () => {
                         <div className="flex justify-center items-center w-[20%] font-['weddingScriptFont'] text-[110px]">26</div>
                         <div className="flex justify-center items-center w-[40%] font-['weddingStarlightFont'] text-[40px] border-b-2 border-t-2">2026</div>
                     </div>
-
                 </div>
 
-                {/* <button className="mt-4 w-[230px] bg-white text-black font-semibold py-2 rounded-lg shadow-md hover:bg-gray-200 transition">
-                    CONFIRMAR ASISTENCIA
-                </button> */}
+
+
+                <div className="w-[50%] sm:w-[20%] flex justify-center items-center gap-2 h-10 cursor-pointer rounded-md shadow-2xl text-white font-semibold bg-[linear-gradient(to_right,rgba(0,0,0,0.5),rgba(255,215,0,0.4),rgba(0,0,0,0.5))] hover:bg-[linear-gradient(to_right,rgba(0,0,0,0.6),rgba(255,215,0,0.5),rgba(0,0,0,0.6))] hover:shadow-xl hover:shadow-yellow-400 hover:scale-105 duration-300 mt-4 backdrop-blur-md border border-white/10">
+                    Más detalles
+                    <svg
+                        className="w-5 h-5"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                            strokeLinejoin="round"
+                            strokeLinecap="round"
+                        ></path>
+                    </svg>
+                </div>
+
 
             </div>
         </header>
